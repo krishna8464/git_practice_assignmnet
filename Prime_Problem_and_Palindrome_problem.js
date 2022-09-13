bbce465c03fe2bd684c9a4369c5c21588117caed
Dinesh Suthar <goga03415@gmail.com>
2022-09-13 21:00:43 +0530

    new solution

--- Prime_Problem_and_Palindrome_problem.js
+++ Prime_Problem_and_Palindrome_problem.js
@@ -1,26 +1,26 @@
 // solution for prime problem
 let num = 8;
 let count = 0;
-for (let i = 2;i<=num;i++){
+for (let i = 1;i<=num;i++){
     if(num%i===0){
         count++;
     }
 }
-if(count ==3){
+if(count ==2){
     console.log(num,"is a prime number");
 }else{
     console.log(num,"is not a prime number");
 }
 
-//solution for palindrom
+//solution for palindrome
 
-let str = zooz;
-let bag = '';
+let str = "zooz";
+let bag = "";
 for(let i = str.length-1;i>=0;i--){
-    bag = bag + str[0];
+    bag = bag + str[i];
 }
-if(str == bag){
-    console.log(str,"is a palindrom");
+if(str === bag){
+    console.log(str,"is a palindrome");
 }else{
-    console.log(str,"is not a palindrom");
+    console.log(str,"is not a palindrome");
 }
